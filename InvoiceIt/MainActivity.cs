f67f8b9188bf50347f7f410629c4185db0c8ceeb
Sebastian Balazote <sbalazote@gmail.com>
2018-09-06 10:06:38 -0300

    wip #2

--- InvoiceIt/MainActivity.cs
+++ InvoiceIt/MainActivity.cs
@@ -12,26 +12,20 @@ using Android.Views;
 using Android.Support.V4.Content;
 using Android.Support.V4.App;
 using System.Collections.Concurrent;
-using Uri = Android.Net.Uri;
 using System.IO;
-using System.Collections;
-using System.Globalization;
-using Android.Content;
 using Google.AR.Core;
 using Google.AR.Core.Exceptions;
 using Android.Content.Res;
 using Android.Graphics;
 using Java.Nio;
-using Camera = Google.AR.Core.Camera;
-using Path = System.IO.Path;
+using Config = Google.AR.Core.Config;
 using Environment = Android.OS.Environment;
-using File = System.IO.File;
 using IOException = Java.IO.IOException;
 
 namespace InvoiceIt
 {
     [Activity(Label = "InvoiceIt", MainLauncher = true, Icon = "@mipmap/bit_logo", Theme = "@style/Theme.AppCompat.NoActionBar", ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize, ScreenOrientation = Android.Content.PM.ScreenOrientation.Locked)]
-    public class MainActivity : AppCompatActivity, GLSurfaceView.IRenderer, Android.Views.View.IOnTouchListener
+    public class MainActivity : AppCompatActivity, GLSurfaceView.IRenderer, View.IOnTouchListener
     {
         private int _mWidth;
         private int _mHeight;
@@ -66,11 +60,6 @@ namespace InvoiceIt
 
             SetContentView(Resource.Layout.Main);
             mSurfaceView = FindViewById<GLSurfaceView>(Resource.Id.surfaceview);
-            Button button = FindViewById<Button>(Resource.Id.fboRecord_button);
-            button.Click += delegate {
-                //button.Text = string.Format("{0} clicks!", count++);
-                this._capturePicture = true;
-            };
             mDisplayRotationHelper = new DisplayRotationHelper(this);
 
             Java.Lang.Exception exception = null;
@@ -78,7 +67,7 @@ namespace InvoiceIt
 
             try
             {
-                mSession = new Session(/*context=*/this);
+                mSession = new Session(this);
             }
             catch (UnavailableArcoreNotInstalledException e)
             {
@@ -119,15 +108,16 @@ namespace InvoiceIt
             AssetManager assets = this.Assets;
             var inputStream = assets.Open("receipts.imgdb");
             AugmentedImageDatabase imageDatabase = AugmentedImageDatabase.Deserialize(mSession, inputStream);
-
+            config.SetFocusMode(Config.FocusMode.Auto);
             config.AugmentedImageDatabase = imageDatabase;
 
             mSession.Configure(config);
 
-            mGestureDetector = new Android.Views.GestureDetector(this, new SimpleTapGestureDetector
+            mGestureDetector = new GestureDetector(this, new SimpleTapGestureDetector
             {
-                SingleTapUpHandler = (MotionEvent arg) => {
-                    onSingleTap(arg);
+                SingleTapUpHandler = (MotionEvent arg) =>
+                {
+                    OnSingleTap(arg);
                     return true;
                 },
                 DownHandler = (MotionEvent arg) => true
@@ -154,7 +144,7 @@ namespace InvoiceIt
             {
                 if (mSession != null)
                 {
-                    showLoadingMessage();
+                    ShowLoadingMessage();
                     // Note that order matters - see the note in onPause(), the reverse applies here.
                     mSession.Resume();
                 }
@@ -164,7 +154,7 @@ namespace InvoiceIt
             }
             else
             {
-                ActivityCompat.RequestPermissions(this, new string[] { Android.Manifest.Permission.Camera }, 0);
+                ActivityCompat.RequestPermissions(this, new[] { Android.Manifest.Permission.Camera }, 0);
             }
         }
 
@@ -192,8 +182,6 @@ namespace InvoiceIt
                 Toast.MakeText(this, "Camera permission is needed to run this application", ToastLength.Long).Show();
                 Finish();
             }
-
-            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
         public override void OnWindowFocusChanged(bool hasFocus)
@@ -202,19 +190,11 @@ namespace InvoiceIt
 
             if (hasFocus)
             {
-                // Standard Android full-screen functionality.
-                //Window.DecorView.SystemUiVisibility = Android.Views.SystemUiFlags.LayoutStable
-                //| Android.Views.SystemUiFlags.LayoutHideNavigation
-                //| Android.Views.SystemUiFlags.LayoutFullscreen
-                //| Android.Views.SystemUiFlags.HideNavigation
-                //| Android.Views.SystemUiFlags.Fullscreen
-                //| Android.Views.SystemUiFlags.ImmersiveSticky;
-
                 Window.AddFlags(WindowManagerFlags.KeepScreenOn);
             }
         }
 
-        private void onSingleTap(MotionEvent e)
+        private void OnSingleTap(MotionEvent e)
         {
             // Queue tap if there is space. Tap is lost if queue is full.
             if (mQueuedSingleTaps.Count < 16)
@@ -227,18 +207,16 @@ namespace InvoiceIt
             GLES20.GlClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
             // Create the texture and pass it to ARCore session to be filled during update().
-            mBackgroundRenderer.CreateOnGlThread(/*context=*/this);
-            if (mSession != null)
-                mSession.SetCameraTextureName(mBackgroundRenderer.TextureId);
+            mBackgroundRenderer.CreateOnGlThread(this);
+            mSession?.SetCameraTextureName(mBackgroundRenderer.TextureId);
 
             // Prepare the other rendering objects.
             try
             {
-                mVirtualObject.CreateOnGlThread(/*context=*/this, "andy.obj", "andy.png");
+                mVirtualObject.CreateOnGlThread(this, "andy.obj", "andy.png");
                 mVirtualObject.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
 
-                mVirtualObjectShadow.CreateOnGlThread(/*context=*/this,
-                    "andy_shadow.obj", "andy_shadow.png");
+                mVirtualObjectShadow.CreateOnGlThread(this, "andy_shadow.obj", "andy_shadow.png");
                 mVirtualObjectShadow.SetBlendMode(ObjectRenderer.BlendMode.Shadow);
                 mVirtualObjectShadow.setMaterialProperties(1.0f, 0.0f, 0.0f, 1.0f);
             }
@@ -249,13 +227,13 @@ namespace InvoiceIt
 
             try
             {
-                mPlaneRenderer.CreateOnGlThread(/*context=*/this, "trigrid.png");
+                mPlaneRenderer.CreateOnGlThread(this, "trigrid.png");
             }
-            catch (Java.IO.IOException e)
+            catch (IOException e)
             {
                 Log.Error(TAG, "Failed to read plane texture");
             }
-            mPointCloud.CreateOnGlThread(/*context=*/this);
+            mPointCloud.CreateOnGlThread(this);
         }
 
         public void OnSurfaceChanged(IGL10 gl, int width, int height)
@@ -275,7 +253,7 @@ namespace InvoiceIt
                 // A tracked image has a match.
                 if (img.TrackingState == TrackingState.Tracking)
                 {
-                    //  TODO save screenshot.
+                    _capturePicture = true;
                 }
             }
         }
@@ -297,8 +275,8 @@ namespace InvoiceIt
                 // Obtain the current frame from ARSession. When the configuration is set to
                 // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
                 // camera framerate.
-                Frame frame = mSession.Update();
-                Camera camera = frame.Camera;
+                var frame = mSession.Update();
+                var camera = frame.Camera;
                 // Handle taps. Handling only one tap per frame, as taps are usually low frequency
                 // compared to frame rate.
                 MotionEvent tap = null;
@@ -371,10 +349,9 @@ namespace InvoiceIt
                 {
                     foreach (var plane in planes)
                     {
-                        if (plane.GetType() == Plane.Type.HorizontalUpwardFacing
-                                && plane.TrackingState == TrackingState.Tracking)
+                        if (plane.GetType() == Plane.Type.HorizontalUpwardFacing && plane.TrackingState == TrackingState.Tracking)
                         {
-                            hideLoadingMessage();
+                            HideLoadingMessage();
                             break;
                         }
                     }
@@ -403,7 +380,7 @@ namespace InvoiceIt
                 }
 
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
                 // Avoid crashing the application due to unhandled exceptions.
                 Log.Error(TAG, "Exception on the OpenGL thread", ex);
@@ -415,65 +392,6 @@ namespace InvoiceIt
                 SavePicture(gl);
             }
         }
-
-        public static Uri GetOutputMediaFile(Context context, string subdir, string name, bool isPhoto, bool saveToAlbum)
-        {
-            subdir = subdir ?? string.Empty;
-
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                var timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss", CultureInfo.InvariantCulture);
-                if (isPhoto)
-                    name = "IMG_" + timestamp + ".jpg";
-                else
-                    name = "VID_" + timestamp + ".mp4";
-            }
-
-            var mediaType = (isPhoto) ? Environment.DirectoryPictures : Environment.DirectoryMovies;
-            var directory = saveToAlbum ? Environment.GetExternalStoragePublicDirectory(mediaType) : context.GetExternalFilesDir(mediaType);
-            using (var mediaStorageDir = new Java.IO.File(directory, subdir))
-            {
-                if (!mediaStorageDir.Exists())
-                {
-                    if (!mediaStorageDir.Mkdirs())
-                        throw new IOException("Couldn't create directory, have you added the WRITE_EXTERNAL_STORAGE permission?");
-
-                    if (!saveToAlbum)
-                    {
-                        // Ensure this media doesn't show up in gallery apps
-                        using (var nomedia = new Java.IO.File(mediaStorageDir, ".nomedia"))
-                            nomedia.CreateNewFile();
-                    }
-                }
-
-                return Uri.FromFile(new Java.IO.File(GetUniquePath(mediaStorageDir.Path, name, isPhoto)));
-            }
-        }
-
-        private static string GetUniquePath(string folder, string name, bool isPhoto)
-        {
-            var ext = Path.GetExtension(name);
-            if (ext == string.Empty)
-                ext = ((isPhoto) ? ".jpg" : ".mp4");
-
-            name = Path.GetFileNameWithoutExtension(name);
-
-            var nname = name + ext;
-            var i = 1;
-            while (File.Exists(Path.Combine(folder, nname)))
-                nname = name + "_" + (i++) + ext;
-
-            return Path.Combine(folder, nname);
-        }
-
-        /*public void OnSavePicture(View view)
-        {
-            // Here just a set a flag so we can copy
-            // the image from the onDrawFrame() method.
-            // This is required for OpenGL so we are on the rendering thread.
-            this._capturePicture = true;
-        }
-*/
 
         /**
         * Call from the GLThread to save a picture of the current frame.
@@ -490,7 +408,7 @@ namespace InvoiceIt
             mGL.GlReadPixels(0, 0, _mWidth, _mHeight, GLES20.GlRgba, GLES20.GlUnsignedByte, buf);
 
             // Create a file in the Pictures/HelloAR album.
-            var file = new Java.IO.File($"{Environment.GetExternalStoragePublicDirectory(Environment.DirectoryPictures)}/HelloAR", "Img" + DateTime.Now + ".png");
+            var file = new Java.IO.File($"{Environment.GetExternalStoragePublicDirectory(Environment.DirectoryPictures)}/{TAG}", "Img" + DateTime.Now + ".png");
 
             // Make sure the directory exists
             if (!file.ParentFile.Exists())
@@ -500,9 +418,9 @@ namespace InvoiceIt
 
             // Convert the pixel data from RGBA to what Android wants, ARGB.
             var bitmapData = new int[pixelData.Length];
-            for (int i = 0; i < _mHeight; i++)
+            for (var i = 0; i < _mHeight; i++)
             {
-                for (int j = 0; j < _mWidth; j++)
+                for (var j = 0; j < _mWidth; j++)
                 {
                     long p = pixelData[i * _mWidth + j];
                     long b = (p & 0x00ff0000) >> 16;
@@ -514,9 +432,9 @@ namespace InvoiceIt
 
             // Convert upside down mirror-reversed image to right-side up normal
             // image.
-            for (int i = 0; i < _mHeight; i++)
+            for (var i = 0; i < _mHeight; i++)
             {
-                for (int j = 0; j < _mWidth; j++)
+                for (var j = 0; j < _mWidth; j++)
                 {
                     ibt.Put((_mHeight - i - 1) * _mWidth + j, buf.Get(i * _mWidth + j));
                 }
@@ -525,30 +443,27 @@ namespace InvoiceIt
             // Create a bitmap.
             Bitmap bmp = Bitmap.CreateBitmap(bitmapData, _mWidth, _mHeight, Bitmap.Config.Argb8888);
 
-            //bmp.EraseColor(Color.Argb(0, 255, 255, 255));
             bmp.CopyPixelsFromBuffer(ibt);
 
-            var fs = new FileStream(file.Path, FileMode.OpenOrCreate);
             // Write it to disk.
-            //FileOutputStream fos = new FileOutputStream(file);
+            var fs = new FileStream(file.Path, FileMode.OpenOrCreate);
             bmp.Compress(Bitmap.CompressFormat.Png, 100, fs);
             fs.Flush();
             fs.Close();
 
         }
 
-        private void showLoadingMessage()
+        private void ShowLoadingMessage()
         {
             this.RunOnUiThread(() =>
             {
-                mLoadingMessageSnackbar = Snackbar.Make(FindViewById(Android.Resource.Id.Content),
-        "Searching for surfaces...", Snackbar.LengthIndefinite);
-                mLoadingMessageSnackbar.View.SetBackgroundColor(Android.Graphics.Color.DarkGray);
+                mLoadingMessageSnackbar = Snackbar.Make(FindViewById(Android.Resource.Id.Content), "Searching for invoices...", Snackbar.LengthIndefinite);
+                mLoadingMessageSnackbar.View.SetBackgroundColor(Color.DarkGray);
                 mLoadingMessageSnackbar.Show();
             });
         }
 
-        private void hideLoadingMessage()
+        private void HideLoadingMessage()
         {
             this.RunOnUiThread(() =>
             {
